5995eb12a173f4f10a409f5c1bbdac7c1d974779
htierney23 <103545747+htierney23@users.noreply.github.com>
2024-08-30 15:47:04 +1000

    Add files via upload
    indentation fixes and removal of unecessary code

--- app.js
+++ app.js
@@ -3,19 +3,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import FooterNavigation from './components/FooterNavigation';
-import MapScreen from './components/MapScreen';
-import TripPlannerScreen from './components/TripPlannerScreen';
-import SaveScreen from './components/SaveScreen';
-import MeScreen from './components/MeScreen';
 import PaymentGateway from './components/PaymentGateway';
 
 const Stack = createNativeStackNavigator();
 
 const routes = [
-  { name: 'Map', component: MapScreen },
-  { name: 'TripPlanner', component: TripPlannerScreen },
-  { name: 'Save', component: SaveScreen },
-  { name: 'Me', component: MeScreen },
   { name: 'Payment', component: PaymentGateway }
 ];
 
@@ -23,13 +15,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <SafeAreaView style={styles.container}>
-        <Text style={styles.fancyText}>Happy Coding EVAT Legends</Text>
         <View style={styles.navigatorContainer}>
-          <Stack.Navigator initialRouteName="Map">
-            {routes.map((route, index) => (
-              <Stack.Screen key={index} name={route.name} component={route.component} />
-            ))}
-          </Stack.Navigator>
           <View style={styles.personalDetails}>
       <View style={[styles.personalDetailsChild, styles.personalLayout]} />
       <Image
@@ -134,16 +120,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  fancyText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 20, // Add margin to separate text from the navigator
-  },
-  navigatorContainer: {
-    flex: 1,
-    width: '100%',
-    paddingHorizontal: 10, // Add padding to the navigator container
-  },
+
   paymentDetailsButtonBtn: {
     color: "#000",
     fontSize: 18,
