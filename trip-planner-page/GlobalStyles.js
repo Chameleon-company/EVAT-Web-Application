449364137d828830629c8d83d3da39268553d0d6
AdrianT <s222275741@deakin.edu.au>
2024-09-11 20:18:16 +1000

    mostly design, needs interactive elements (buttons, locations as item)

---
+++ trip-planner-page/GlobalStyles.js
@@ -0,0 +1,49 @@
+/* fonts */
+export const FontFamily = {
+  singleLineBodyBase: "Inter-Regular",
+  interSemiBold: "Inter-SemiBold",
+  interBold: "Inter-Bold",
+};
+/* font sizes */
+export const FontSize = {
+  singleLineBodyBase_size: 16,
+  size_xs: 12,
+  size_smi: 13,
+  size_sm: 14,
+  size_mini: 15,
+};
+/* Colors */
+export const Color = {
+  colorDodgerblue: "#3897ff",
+  colorWhite: "#fff",
+  colorGray_100: "#808080",
+  colorGray_200: "#757575",
+  colorGray_300: "rgba(255, 255, 255, 0)",
+  colorGoldenrod: "#cdac00",
+  colorRoyalblue_200: "#007bff",
+  colorRoyalblue_100: "#007aff",
+  colorBlack: "#000",
+  colorGreen: "#008000",
+};
+/* Gaps */
+export const Gap = {
+  gap_xs: 1,
+  gap_sm: 2,
+  gap_md: 3,
+  gap_lg: 8,
+};
+/* Paddings */
+export const Padding = {
+  p_23xl: 42,
+  p_8xs: 5,
+  p_11xs: 2,
+  p_5xs: 8,
+  p_3xs: 10,
+  p_9xs: 4,
+};
+/* border radiuses */
+export const Border = {
+  br_12xs: 1,
+  br_10xs: 3,
+  br_mid_5: 18,
+};
